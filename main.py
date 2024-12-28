b2343da797295ab0dfe7fd1e4a1c6749de2015ea
sg296858 <you@example.com>
2024-12-28 21:48:43 +0530

    DataTransformationStageCompleted

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 
 
 
@@ -20,6 +21,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    data_validation= DataValidationTrainingPipeline()
    data_validation.initiate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e 
+
+STAGE_NAME="Data Transformation Stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   data_transformation= DataTransformationTrainingPipeline()
+   data_transformation.initiate_data_transformation()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/datascience/config/configuration.py
+++ src/datascience/config/configuration.py
@@ -1,7 +1,7 @@
 from src.datascience.constants import *
 from src.datascience.utils.common import read_yaml, create_directories
 from src.datascience.entity.config_entity import (DataIngestionConfig)
-from src.datascience.entity.config_entity import (DataValidationConfig)
+from src.datascience.entity.config_entity import (DataValidationConfig,DataTransformationConfig)
 
 
 class ConfigurationManager:
@@ -42,5 +42,16 @@ class ConfigurationManager:
             unzip_data_dir = config.unzip_data_dir,
             all_schema=schema,
         )
-
         return data_validation_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
