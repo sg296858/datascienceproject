9cb67754270f5d03581b7771d70b345405e39468
sg296858 <you@example.com>
2024-12-28 20:07:56 +0530

    DataValidationCompleted

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 
 
@@ -12,3 +13,14 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+STAGE_NAME="Data Validation Stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   data_validation= DataValidationTrainingPipeline()
+   data_validation.initiate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e 
